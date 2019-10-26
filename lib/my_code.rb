13e586d02d105596a8b9a31d40c624caae6be4d7
Allen Shin <geeuho@gmail.com>
2019-10-26 21:24:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,56 @@
 # My Code here....
+def map_to_negativize(array)
+  i = 0 
+    while i < array.length do
+      array[i] = array[i] * -1
+      i += 1
+    end
+  array
+end
+
+def map_to_no_change(array)
+  i = 0 
+    while i < array.length do
+      array[i] = array[i]
+      i += 1
+    end
+  array
+end
+    
+def map_to_double(array)
+  i = 0 
+    while i < array.length do
+      array[i] = array[i] * 2
+      i += 1
+    end
+  array
+end
+    
+    
+def map_to_square(array)
+  i = 0 
+    while i < array.length do
+      array[i] = array[i] ** 2
+      i += 1
+    end
+  array
+end
+
+def reduce_to_total(array)
+  i = 0 
+  total = 0
+    while i < array.length do
+      total = total + array[i]
+      i += 1
+    end
+  total 
+end
+    
+def reduce_to_all_true(array)
+  array.all?(true)
+end
+
+def reduce_to_any_true(array)
+  array.any?(true) 
+end
+    
